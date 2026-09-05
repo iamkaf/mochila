c886f4649bb46f572fd5e5af27973e019fb784cf
Kaf <coffeechopdev@gmail.com>
2026-09-05 20:59:42 -0300

    Align the explicit Gradle runner version

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 teakit {
-    runnerVersion.set("0.13.2")
+    runnerVersion.set("0.16.0")
     testDirectories.add("test/teakit")
     timeoutSeconds.set(720)
     failOnRuntimeIncomplete.set(true)
